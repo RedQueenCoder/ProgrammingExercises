b6ee1e86d5ed4837b6f44944bc4130321252f385
Matteo Piombo <matteo.piombo@fedro.org>
2016-01-31 07:01:49 +0100

    Document Helper functions
    Now we can Alt-Click on our helper functions to see some documentation.

--- Exercise1:SayingHello/Exercise1:SayingHello/HelperFunctions.swift
+++ Exercise1:SayingHello/Exercise1:SayingHello/HelperFunctions.swift
@@ -8,12 +8,20 @@
 
 import Foundation
 
+
 extension String {
+    
+    /// Trims white spaces and new line characters.
+    ///
+    /// - Returns: A trimmed copy of itself.
     func trim () -> String {
         return self.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet())
     }
 }
 
+/// Validates and trim an input string that should be a name.
+/// - Parameter name: The input name string.
+/// - Returns: The trimmed input name or a string describing the error to the user.
 func nameFunction(name:String) -> String {
     
     let trimmedName = name.trim()
